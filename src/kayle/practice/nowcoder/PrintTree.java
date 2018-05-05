df6d01242491d9263d7db5ff44c3116949ae62b3
Kayle <seiulaomi@126.com>
2018-05-05 18:41:34 +0800

    按之字型打印二叉树

--- src/kayle/practice/nowcoder/PrintTree.java
+++ src/kayle/practice/nowcoder/PrintTree.java
@@ -26,21 +26,22 @@ public class TreeNode {
 * 所以利用栈的特性，用栈来层次遍历，按奇数偶数层不同来进行不同的push次序。
 * 每次一行遍历完后把tmpList添加到resList里面。
 * */
+
 public class PrintTree {
     public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
         ArrayList<ArrayList<Integer>> resList = new ArrayList<ArrayList<Integer>>();
         //存放奇数层的节点
-        Stack s1 = new Stack();
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
         s1.push(pRoot);
         //存放偶数层的节点，并且是从右往左放
-        Stack s2 = new Stack();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
         int layer = 1;
 
         while (!s1.empty() || !s2.empty()) {
             if(layer % 2 != 0) {
                 ArrayList<Integer> tmpList = new ArrayList<Integer>();
                 while (!s1.empty()) {
-                    TreeNode tmpNode = (TreeNode) s1.pop();
+                    TreeNode tmpNode = s1.pop();
                     if(tmpNode != null) {
                         tmpList.add(tmpNode.val);
                         //偶数行应该是从右往左，但是由于栈是先进后出，所以进栈应该从左往右进
@@ -56,7 +57,7 @@ public class PrintTree {
             } else {
                 ArrayList<Integer> tmpList = new ArrayList<Integer>();
                 while (!s2.empty()) {
-                    TreeNode tmpNode = (TreeNode) s2.pop();
+                    TreeNode tmpNode = s2.pop();
                     if(tmpNode != null) {
                         tmpList.add(tmpNode.val);
                         //偶数行应该是从左往右，但是由于栈是先进后出，所以进栈应该从右往左进
