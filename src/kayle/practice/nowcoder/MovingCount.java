d4443a266fe34f5771e869a930413be4c6c1af25
Kayle <seiulaomi@126.com>
2018-05-08 16:42:29 +0800

    机器人的运动范围

--- src/kayle/practice/nowcoder/MovingCount.java
+++ src/kayle/practice/nowcoder/MovingCount.java
@@ -1,5 +1,19 @@
 package kayle.practice.nowcoder;
 
+/*
+* 题目描述:
+* 地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，
+* 每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。
+* 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+* 但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+*
+* 解题思路:
+* 写一个计算位数和的函数，然后用回溯的思想从0，0开始
+* 用一个visited数组来记录是否访问过
+* 越界或者行列坐标数位之和大于threshold或者已被访问过，不计数，返回0
+* 否则先将visited设为true
+* 然后再+1 再加上其他四个方向的递归。
+* */
 public class MovingCount {
     public int movingCount(int threshold, int rows, int cols) {
         boolean[][] visited = new boolean[rows][cols];
