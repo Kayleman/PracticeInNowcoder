900b39b08fc3daee70b5589624a16ea0f6e1c268
Kayle <seiulaomi@126.com>
2018-08-30 11:23:04 +0800

    2018_8_30

---
+++ src/algorithm/nowcoder_algorithm/test5_9.java
@@ -0,0 +1,52 @@
+package algorithm.nowcoder_algorithm;
+
+public class test5_9 {
+    public boolean isPalindrome(ListNode pHead) {
+        // write code here
+        ListNode pSlow = pHead;
+        ListNode pFast = pHead;
+        //长度是奇数，slow指向中间结点
+        //长度是偶数，slow指向中间右侧节点
+        while (pFast != null && pFast.next != null) {
+            pFast = pFast.next.next;
+            pSlow = pSlow.next;
+        }
+        //从中间或中间右侧开始反转链表
+        ListNode pPre = null;
+        while (pSlow != null) {
+            ListNode pNext = pSlow.next;
+            pSlow.next = pPre;
+            pPre = pSlow;
+            pSlow = pNext;
+        }
+
+        ListNode pCur = pHead;
+        while (pPre != null) {
+            if(pCur.val == pPre.val) {
+                pCur = pCur.next;
+                pPre = pPre.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        //ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(2);
+        ListNode node7 = new ListNode(1);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node5;
+        //node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        test5_9 t = new test5_9();
+        System.out.println(t.isPalindrome(node1));
+    }
+}
